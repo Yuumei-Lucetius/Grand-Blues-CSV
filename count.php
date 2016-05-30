143feac0cdab43f816a9d1b36c64b8d480865269
Yuumei <narshe.nashley@gmail.com>
2016-05-30 16:35:04 +1000

    Update count.php
    Added radio buttons for filtering stopwords (additionally displaying the most frequently used word) and fields for replacing text.

--- count.php
+++ count.php
@@ -31,8 +31,38 @@
 					<blockquote>
 						<div id="char_count"></div>
 						<div id="word_count"></div>
+						<div id="common_word"></div>
 					</blockquote>
-					<textarea id="text_counter" class="form-control" onkeyup="count(this)" rows="10"></textarea>
+					<textarea id="text_counter" class="form-control" onkeyup="count()" rows="10" style="width:600px;"></textarea>
+					<div class="form-group">
+						<div class="form-inline">
+							<label for="originalText">Replace:</label>
+							<input id="originalText" type="text" class="form-control">
+							<label for="newText">With:</label>
+							<input id="newText" type="text" class="form-control">
+						</div>
+						<div class="form-inline">
+							<label class="radio-inline">
+								<input id="embdtxt1" type="radio" name="embdtxt" value="embd1" checked="checked">Include embedded text
+							</label>
+							<label class="radio-inline">
+								<input id="embdtxt2" type="radio" name="embdtxt" value="embd2">Exclude embedded text
+							</label>
+						</div>
+						<button class="btn btn-default" onclick="replaceText(); return false;">Replace</button>
+						<div id="stopdiv" class="form-inline">
+							<label>Filter:</label>
+							<label class="radio-inline">
+								<input id="stopwords1" type="radio" name="stopwords" onclick="count()" value="sword1">Basic Stopwords
+							</label>
+							<label class="radio-inline">
+								<input id="stopwords2" type="radio" name="stopwords" onclick="count()" value="sword2">All Stopwords
+							</label>
+							<label class="radio-inline">
+								<input id="stopwords0" type="radio" name="stopwords" onclick="count()" value="sword0">None
+							</label>
+						</div>
+					</div>
 				</form>
 				</div>
 			</div>
