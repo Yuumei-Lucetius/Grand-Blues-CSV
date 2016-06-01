2943107d1e92e6bb8b0b6f2b125322b7b232e6dd
Yuumei <narshe.nashley@gmail.com>
2016-06-01 17:30:35 +1000

    Add files via upload

---
+++ js/gbfcomic.js
@@ -0,0 +1,97 @@
+var comicPos = 1;
+
+//Load page according to its number
+function load_comic(pos) {
+	$("#comic").empty();
+	$("#comic").append("<img style='-webkit-user-select: none; max-width: 100%;' src='http://gbf.game-a1.mbga.jp/assets_en/img/sp/assets/comic/episode/episode_" + pos + ".jpg'></img>");
+}
+
+//Load previous page
+function comic_back() {
+	if (comicPos > 1) {
+		comicPos -= 1;
+		load_comic(comicPos);
+		if (comicPos == 1 && !$("#prev").hasClass("disabled")) {
+			$("#prev").addClass("disabled");
+		}
+	}
+}
+
+//Load next page
+function comic_next() {
+	comicPos += 1;
+	load_comic(comicPos);
+	if (comicPos > 1 && $("#prev").hasClass("disabled")) {
+		$("#prev").removeClass("disabled");
+	}
+}
+
+//Converts an integer to a string with three digits
+function detect_digits(n) {
+	//n is 1 digit
+	if (n < 10) {
+		n = "00" + n.toString();
+	} 
+	//n is 2 digits
+	else if (n < 100) {
+		n = "0" + n.toString();
+	} 
+	//n is 3 digits
+	else {
+		n = n.toString();
+	}
+	
+	return n;
+}
+
+function open_menu() {
+	var thumbPos = detect_digits(comicPos);
+	var viewer_menu =
+	'<div id="menu" class="modal fade">' +
+	'	<div class="modal-dialog">' +
+	'		<div class="modal-content">' +
+	'			<div class="modal-header text-center">' +
+	'        		<button type="button" class="close" data-dismiss="modal">&times;</button>' +
+	'      		</div>' +
+	'			<div class="modal-body text-center">' +
+	'				<p><strong>Currently viewing strip #' + comicPos + '</strong></p>' +
+	'				<img src="http://gbf.game-a1.mbga.jp/assets_en/img/sp/assets/comic/thumbnail/thum_00' + thumbPos + '.png"></img>' +
+	'				<p><strong>Designate a strip:</strong></p>' +
+	'				<form class="form-inline">' +
+	'					<div class="input-group">' +
+	'						<div class="input-group-addon">#</div>' +
+	'						<input type="text" id="stripNo" class="form-control" placeholder="Strip">' +
+	'					</div>' +
+	'					<button class="btn btn-primary" onclick="custom_load(); return false;">Load</button>' +
+	'				</form>' +
+	'   	 	</div>' +
+	'			<div class="modal-footer">' +
+	'    		</div>' +
+	'      	</div>' +
+	'	</div>' +
+	'</div>';
+	$(viewer_menu).modal();
+}
+
+function custom_load() {
+	alert($("#stripNo").val());
+}
+
+$(function() {
+	load_comic(comicPos);
+	
+	//Transition between pages using keystrokes (currently only the left and right arrow keys are used)
+	$(document).keydown(function(e) {
+		if (e.keyCode == 37) {
+			if ($("#menu").data('bs.modal')) {
+				$('.modal').modal('hide');
+			}
+			comic_back();
+		} else if (e.keyCode == 39) {
+			if ($("#menu").data('bs.modal')) {
+				$('.modal').modal('hide');
+			}
+			comic_next();
+		}
+	});
+});
